6b0ed80726b1cf4cd0fe128e653087e28f438c8c
Orion <orionsolidified@gmail.com>
2023-01-07 09:19:15 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,136 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const maxRetries = 20;
+  const [input, setInput] = useState('poster of Valorie Curry | standing alone on hill| centered| detailed gorgeous face| anime style| key visual| intricate detail| highly detailed| breathtaking| vibrant| panoramic| cinematic| Carne Griffiths| Conrad Roset| Makoto Shinkai');
+  const [img, setImg] = useState('');
+  const [retry, setRetry] = useState(0);
+  const [retryCount, setRetryCount] = useState(maxRetries);
+  const [isGenerating, setIsGenerating] = useState(false);
+  // Add new state here
+  const [finalPrompt, setFinalPrompt] = useState('');
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const generateAction = async () => {
+    console.log('Generating...');
+
+    if (isGenerating && retry === 0) return;
+
+    setIsGenerating(true);
+
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0;
+        } else {
+          return prevState - 1;
+        }
+      });
+
+      setRetry(0);
+    }
+
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input }),
+    });
+
+    const data = await response.json();
+
+    if (response.status === 503) {
+      setRetry(data.estimated_time);
+      return;
+    }
+
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      setIsGenerating(false);
+      return;
+    }
+
+    // Set final prompt here
+    setFinalPrompt(input);
+    // Remove content from input box
+    setInput('');
+    setImg(data.image);
+    setIsGenerating(false);
+  };
+
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(`Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`);
+        setRetryCount(maxRetries);
+        return;
+        }
+
+      console.log(`Trying again in ${retry} seconds.`);
+
+      await sleep(retry * 1000);
+
+      await generateAction();
+    };
+
+    if (retry === 0) {
+      return;
+    }
+
+    runRetry();
+  }, [retry]);
+
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>Kara Gen | buildspace</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Who is better Kara or Valorie Curry</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>Turn Kara into anyone you want! Make sure you refer to me as "kara" in the prompt</h2>
+          </div>
+          {/* Add prompt container here */}
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange} />
+            <div className="prompt-buttons">
+              {/* Tweak classNames to change classes */}
+              <a
+                className={
+                  isGenerating ? 'generate-button loading' : 'generate-button'
+                }
+                onClick={generateAction}
+              >
+                {/* Tweak to show a loading indicator */}
+                <div className="generate">
+                  {isGenerating ? (
+                    <span className="loader"></span>
+                  ) : (
+                    <p>Generate</p>
+                  )}
+                </div>
+              </a>
+            </div>
           </div>
         </div>
+        {/* Add output container */}
+        {img && (
+  <div className="output-content">
+    <Image src={img} width={512} height={512} alt={finalPrompt} />
+    {/* Add prompt here */}
+    <p>{finalPrompt}</p>
+  </div>
+)}
       </div>
       <div className="badge-container grow">
         <a
